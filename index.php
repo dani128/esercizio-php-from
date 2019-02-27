1ff89dc4d71df21291e28eec6f209514130dd382
dani128 <44221952+dani128@users.noreply.github.com>
2019-02-27 10:14:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<!--style.css-->
+    <link rel="stylesheet" href="style.css">
+    
+
+    <tittle>from </tittle>
+    </head>
+    <body>
+      
+<div class="container">
+ <h1>Inserisci le tue credenziali</h1>
+<form> <div class="form-group">
+    <label for="exampleInputEmail1">Nome</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">Inserisci il tuo nome</small>
+  </div>
+    
+  </form>
+</div>
+
+<div class="container">
+<form> <div class="form-group">
+    <label for="exampleInputEmail1">Cognome</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">Inserisci il tuo Cognome</small>
+  </div>
+    
+  </form>
+</div>
+
+
+
+
+
+    </body>
+     <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src=js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html
