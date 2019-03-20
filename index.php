6493398b607ee27c23d26bdabc073281fb94b188
dani128 <44221952+dani128@users.noreply.github.com>
2019-03-20 10:11:02 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+$nome = (isset($_POST['nome'])) ? $_POST['nome'] : '';
+$Cognome = (isset($_POST['Cognome'])) ? $_POST['Cognome'] : '';
+$Indirizzo = (isset($_POST['Indirizzo'])) ? $_POST['Indirizzo'] : '';
+print $nome;
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,34 +18,48 @@
     <link rel="stylesheet" href="style.css">
     
 
-    <tittle>from </tittle>
+    
     </head>
     <body>
       
+    <div class="container">
+    <div class="alert alert-primary">
+    </div>
+<?php 
+   foreach ($_POST as $key => $value) {
+   print $key . ': ' . $value . '<br>';
+   }
+?>
+
 <div class="container">
  <h1>Inserisci le tue credenziali</h1>
 <form> <div class="form-group">
     <label for="exampleInputEmail1">Nome</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nome" placeholder="Inserisci Nome">
     <small id="emailHelp" class="form-text text-muted">Inserisci il tuo nome</small>
   </div>
-    
+   
   </form>
 </div>
 
 <div class="container">
 <form> <div class="form-group">
     <label for="exampleInputEmail1">Cognome</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"name="Cognome" placeholder="Inserisci Cognome">
     <small id="emailHelp" class="form-text text-muted">Inserisci il tuo Cognome</small>
   </div>
-    
+     
   </form>
 </div>
 
+<div class="container">
+<form> <div class="form-group">
+    <label for="exampleInputEmail1">Indirizo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"name="Indirizzo" placeholder="Inserisci Indirizzo">
+    <small id="emailHelp" class="form-text text-muted">Inserisci il tuo Indirizzo</small>
+  </div>
 
-
-
+<button type="Invia" class="btn btn-primary">Invia</button>
 
     </body>
      <script src="js/jquery-3.3.1.slim.min.js"></script>
